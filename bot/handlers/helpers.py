8b838d211e535d9fcf464737f34cdef3b98a8862
anekobtw <krolexonder@gmail.com>
2025-04-04 15:58:34 +0400

    Update helpers.py

--- bot/handlers/helpers.py
+++ bot/handlers/helpers.py
@@ -1,6 +1,4 @@
-import asyncio
 from datetime import datetime
-from typing import Any
 
 import pymorphy3
 from anilibria.models import Anime
@@ -11,29 +9,12 @@ from enums import API, AnimeInfo
 morph = pymorphy3.MorphAnalyzer()
 
 
-async def fetch_anime_data(anime: Anime) -> tuple[Any | None, Any]:
-    async def get_jutsu() -> Any | None:
-        try:
-            return (await asyncio.to_thread(API.jutsu.value.search, anime.name_ru))[0]
-        except IndexError:
-            return None
-
-    async def get_jikan() -> Any:
-        try:
-            result = await asyncio.to_thread(API.jikan.value.search, search_type="anime", query=anime.name_en)
-            return result["data"][0]
-        except IndexError:
-            return None
-
-    jutsu, jikan = await asyncio.gather(get_jutsu(), get_jikan())
-
-    return jutsu, jikan
-
-
 async def generate_description(anime: Anime) -> str:
-    jutsu, jikan = await fetch_anime_data(anime)
+    jutsu = API.jutsu.value.search(anime.name_ru)[0]
+    jikan = API.jikan.value.search(search_type="anime", query=anime.name_en)["data"][0]
 
-    return "".join(AnimeInfo.DESCRIPTION.value).format(
+
+    description = AnimeInfo.DESCRIPTION.value.format(
         name=anime.name_ru,
         year=f"{anime.season.capitalize()} {anime.year}",
         age=f", {jutsu.age}+" if jutsu else "",
@@ -42,11 +23,13 @@ async def generate_description(anime: Anime) -> str:
         genres=", ".join(anime.genres),
         status=anime.status,
         type=anime.type,
-        description=f"{".".join(anime.description.split(".")[:3])}...",
+        description=f"{'.'.join(anime.description.split('.')[:3])}...",
         trailer=jikan["trailer"]["url"],
         link_jutsu=f"https://jut.su/{jutsu.name.id}" if jutsu else None,
         link_anilibria="https://www.anilibria.top/anime/releases/release/" + anime.code + "/episodes",
     )
+
+    return description
 
 
 def pluralize(word: str, number: int) -> str:
