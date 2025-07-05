c248d9c30c56c4e3d81a5e68411fae2407203b1a
anekobtw <krolexonder@gmail.com>
2025-07-05 21:26:11 +0400

    manga.py -> anime.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 
-from . import images, start, manga
+from . import images, start, anime
 
 router = Router()
 router.include_router(start.router)
 router.include_router(images.router)
-router.include_router(manga.router)
+router.include_router(anime.router)
